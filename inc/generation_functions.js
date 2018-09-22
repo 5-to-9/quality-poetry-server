80ed66a047a341ae1a291290c3589c3042e189f9
Daniel Max <danielmaxsiegel@gmail.com>
2018-09-22 00:15:21 -0700

    file read happening through async

--- inc/generation_functions.js
+++ inc/generation_functions.js
@@ -1,3 +1,8 @@
+var fs = require('fs');
+var readline = require('readline');
+const doAsync = require('doasync');
+
+
 module.exports = {
   generateBasic: function (type) {
     return generatePoem(type);
@@ -8,16 +13,49 @@ module.exports = {
 };
 
 var generatePoem = function (type) {
-  var dictionary = require("../src/dictionary");
+  var dictionary = readFiles();
 
   var result = {
     "poem_type": type,
     "line1": "hello there",
     "line2": "this is a poem"
   };
+
+  // doAsync(fs).readFile('./src/dictionary/verb.txt','utf8')
+  //   .then((data) => console.log(data))
+  //   .then((data) => dictionary = data)
+  //   .then((dictionary) => console.log(dictionary));
+
+  doAsync(fs).readdir("./src/dictionary")
+    .then((data) => console.log(data))
+    .then((data) => (result = data));
+
   return result;
 }
 
-var readFiles = function (){
+function readFiles() {
+  var path = "./src/dictionary";
+  var dict = {};
+  var j=0;
 
+  var filename = path + "/verb.txt";
+
+  fs.readdir(path, function(err, items) {
+    for (var i = 0; i < items.length; ++i) {
+      var filename = path + '/' + items[i];
+      readline.createInterface({
+        input: fs.createReadStream(filename),
+        terminal: false
+      }).on('line', function(line) {
+        dict[j] = line;
+        ++j;
+      });
+    }
+  });
+
+  // console.log(dict);
+
+  // return dict;
+
+  // onReadComplete();
 }
--- src/dictionary/index.js
+++ src/dictionary/index.js
@@ -2,9 +2,15 @@ var normalizedPath = require("path").join(__dirname, "routes");
 
 var fs = require('fs');
 
-fs.readFile('./phrases.txt', 'utf8', function(err, data) {
-    // if (err) throw err;
-    // console.log("Hey");
+// fs.readFile('./phrases.txt', 'utf8', function(err, data) {
+var path = "./src/dictionary/";
+
+fs.readdir(path, function(err, items) {
+    for (var i=0; i<items.length; i++) {
+      if(items[i].slice(-4) == ".txt"){
+        console.log(items[i]);
+      }
+    }
 });
 
 // require("fs").readdirSync(normalizedPath).forEach(function(file) {
