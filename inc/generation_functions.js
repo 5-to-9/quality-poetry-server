7ec8466300b3263f63c4e801ea735031b425f309
Daniel Max <danielmaxsiegel@gmail.com>
2018-09-24 00:18:12 -0700

    dictionary gets correct words based on randomly-assigned gender

--- inc/generation_functions.js
+++ inc/generation_functions.js
@@ -13,24 +13,61 @@ module.exports = {
 
 function generatePoem (type, callback) {
   var poem = { "type":type };
-  var genderProb = Math.floor(Math.random() * 2) + 1;
 
   var dirPath = "./src/dictionary";
   var phrasesFile = dirPath + "/phrases.json";
   var dictionaryFile = dirPath + "/dictionary.json";
 
   fs.readFile(phrasesFile, 'utf8', function(phraseErr, phrases) {
-    fs.readFile(dictionaryFile, 'utf8', function(dictErr, words) {
-
+    fs.readFile(dictionaryFile, 'utf8', function(dictErr, dictionary) {
       var lineCount = getLineCount(Math.floor(Math.random() * 10) + 1);
+      var genderProb = Math.floor(Math.random() * 3) + 1;
+      var phraseToGet;
 
-      for(var i=0; i<lineCount;++i){
-        poem["line" + i] = "line " + i;
+      dictionary = JSON.parse(dictionary);
+      phrases = JSON.parse(phrases);
+
+      if(genderProb == 1){
+        dictionary.pronoun_subjective = "he";
+        dictionary.pronoun_objective = "him";
+        dictionary.pronoun_possessive = "his";
+      } else if(genderProb == 2){
+        dictionary.pronoun_subjective = "she";
+        dictionary.pronoun_objective = "her";
+        dictionary.pronoun_possessive = "her";
+      } else {
+        dictionary.pronoun_subjective = "they";
+        dictionary.pronoun_objective = "them";
+        dictionary.pronoun_possessive = "their";
       }
+
+      for(var i = 1; i <= lineCount; ++i){
+        if(i == 1){
+          phraseToGet = "beginning";
+        } else if (i == lineCount){
+          phraseToGet = "end";
+        } else {
+          phraseToGet = "middle";
+        }
+
+        poem["line" + i] = generateLine(phraseToGet, phrases, dictionary);
+      }
+
       return callback(poem);
     })
   });
 }
+
+function generateLine(phraseToGet, phrases, dictionary){
+  var basicLine = randomVal(phrases[phraseToGet]);
+
+  return basicLine;
+}
+
+function randomVal (obj) {
+    var keys = Object.keys(obj)
+    return obj[keys[ keys.length * Math.random() << 0]];
+};
 
 function getLineCount(lineProb){
   if(lineProb < 4){
